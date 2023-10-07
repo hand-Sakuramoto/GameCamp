168b9888109132c6677364ac8db5246aa34be8bc
YuuOgasawara <90016082+YuuOgasawara@users.noreply.github.com>
2023-10-07 12:55:37 +0900

    テスト用プレイヤー移動処理(仮)完了(10/7 12:55)

--- Assets/Scripts/Ogasawara/OgasaScripts/PlayerScripts.cs
+++ Assets/Scripts/Ogasawara/OgasaScripts/PlayerScripts.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerScripts : MonoBehaviour
 {
     [Header("プレイヤー移動速度")] public float PlayerSpeed;
-    [Header("ミントの数")] public float MintNum = 0;
+    [Header("持っているミントの数")] public float MintNum = 0;
     [Header("プレイヤーの体力")] public int PlayerHP;
 
     private Rigidbody rb; //RigidBody宣言　(10/7 12:22)
 
 
-
+    private int PlayableNum = 0; //変数によるプレイヤー操作可能タイミング制限（0が操作可能、1が準備、ゲーム終了時など操作不能時）
 
 
 
@@ -25,6 +25,34 @@ public class PlayerScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(PlayableNum == 0)
+        {
+            //移動処理(10/7 12:39)
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                rb.velocity = transform.right * PlayerSpeed;
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                rb.velocity = transform.right * -PlayerSpeed;
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                rb.velocity = transform.forward * PlayerSpeed;
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                rb.velocity = transform.forward * -PlayerSpeed;
+            }
+
+            if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            {
+                rb.velocity = Vector3.zero;
+            }
+            //移動ここまで
+        }
     }
 }
