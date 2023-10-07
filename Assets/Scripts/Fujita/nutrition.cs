a948f36b3ac75ad371f7d842f6b1202fd9146bac
YuichiFujita <221065@st.yoshida-g.ac.jp>
2023-10-07 18:24:44 +0900

    かけらと栄養剤の取得時のplayer関数呼び出し設定

--- Assets/Scripts/Fujita/nutrition.cs
+++ Assets/Scripts/Fujita/nutrition.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class nutrition : MonoBehaviour
 {
 	// メンバ変数を宣言
-	[SerializeField] public Object thisObject;   // 自身のオブジェクト
+	[SerializeField] public Object thisObject;	// 自身のオブジェクト
+	[SerializeField] public PlayerScripts PS;	// プレイヤープレハブ
 
 	// Start is called before the first frame update
 	void Start()
@@ -30,6 +31,9 @@ public class nutrition : MonoBehaviour
 
 			// 自身を破棄
 			Destroy(thisObject);
+
+			// 栄養剤バフによるミント増殖スピードアップ
+			PS.AutoMintUpSpeedUp();
 		}
 	}
 }
