c013dcbfd7298e6de64485901e788a6c2e9a40e9
hand-Sakuramoto <sakuramoto_daiki@hand.co.jp>
2023-10-08 02:48:48 +0900

    カメラ追尾機能追加

---
+++ Assets/Scripts/Sakuramoto/CameraManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Camera m_Camera;
+
+    [SerializeField]
+    Vector3 m_Offset = Vector3.zero;
+
+    PlayerScripts m_PlayerScripts;
+
+    private void Awake()
+    {
+        m_Camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        m_PlayerScripts = PlayerScripts.Instance;
+    }
+
+    void Update()
+    {
+        m_Camera.transform.position = Vector3.Lerp(m_Camera.transform.position, m_PlayerScripts.transform.position + m_Offset, 6.0f * Time.deltaTime);
+    }
+}
