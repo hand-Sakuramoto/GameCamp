ab670a8ca4381dab61d59a74205db11f03cf60ff
szkkit <32424014+szkkit@users.noreply.github.com>
2023-10-07 18:27:15 +0900

    Sceneのスクリプト追加

---
+++ Assets/Scripts/Suzuki/ResultScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+/*MainScene上に増設
+using UnityEngine.SceneManagement;
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+*/
+
+}
