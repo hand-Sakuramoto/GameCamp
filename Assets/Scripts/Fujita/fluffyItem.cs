46b7d244ae83fb2945462e1e174bc709d80c900c
YuichiFujita <221065@st.yoshida-g.ac.jp>
2023-10-07 19:34:34 +0900

    アイテムの回転処理を経過時間で回すように変更

--- Assets/Scripts/Fujita/fluffyItem.cs
+++ Assets/Scripts/Fujita/fluffyItem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class fluffyItem : MonoBehaviour
 {
 	// 変数を宣言
-	[Header("アイテムの向き加算量")] public float fAddRotation = 0.0f;	// アイテム向き加算量
+	[Header("アイテムの向き加算量")] public float fRotationSpeed = 0.0f;	// アイテム向き加算量
 
 	// Start is called before the first frame update
 	void Start()
@@ -20,7 +20,7 @@ public class fluffyItem : MonoBehaviour
 		Vector3 rot = transform.rotation.eulerAngles;  // 向き
 
 		// 向きを加算
-		rot.y += fAddRotation;
+		rot.y += Time.deltaTime * fRotationSpeed;
 
 		// 向きを反映
 		transform.rotation = Quaternion.Euler(rot);
