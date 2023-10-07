3b01952f8f873af3f376c66ba4c14ddb4c0c6874
YuichiFujita <221065@st.yoshida-g.ac.jp>
2023-10-07 16:58:35 +0900

    nutrition:クラス名修正 fluffyItem:アイテムをふわふわさせる処理

--- Assets/Scripts/Fujita/nutrition.cs
+++ Assets/Scripts/Fujita/nutrition.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class nutrition : MonoBehaviour
 {
 	// メンバ変数を宣言
-	[SerializeField] public Object thisObject;	// 自身のオブジェクト
+	[SerializeField] public Object thisObject;   // 自身のオブジェクト
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 
 	// 当たり判定
 	void OnTriggerEnter(Collider other)
@@ -31,5 +31,5 @@ public class NewBehaviourScript : MonoBehaviour
 			// 自身を破棄
 			Destroy(thisObject);
 		}
-	} 
+	}
 }
