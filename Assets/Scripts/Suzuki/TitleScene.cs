00e1afe2071da8131105768f750b5eb6de716aa8
szkkit <32424014+szkkit@users.noreply.github.com>
2023-10-07 19:30:41 +0900

    タイトルのSE対応

--- Assets/Scripts/Suzuki/TitleScene.cs
+++ Assets/Scripts/Suzuki/TitleScene.cs
@@ -5,22 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class TitileScene : MonoBehaviour
 {
+
+    public AudioClip sound;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKey ){
-           ChangeScene();
+        if(Input.anyKey){
+           audioSource.PlayOneShot(sound);//SEを鳴らす
+           Invoke("ChangeScene", 0.25f);//シーンをまたぐとSEが破棄され、途切れるため、少し待つ対処をとる。
         }
     }
 
     void ChangeScene()
     {
-        SceneManager.LoadScene("Result");
+        SceneManager.LoadScene("Result");//メインシーンに遷移させる
     }
 
 }
